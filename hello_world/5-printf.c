cf84afe2d295846d1dfe638d16ee7d83063c3541
Obed Rayo <obedrayo2356@gmail.com>
2022-10-09 09:32:53 -0700

    Add 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /*
-Write a C program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line.
-*/
+ *Write a C program that prints a text with printf
+ */
 
 int main(void)
 {
