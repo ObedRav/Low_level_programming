d08a4ef7a8458442e64031a7a13475f030aa3de7
Obed Rayo <obedrayo2356@gmail.com>
2022-10-09 09:26:18 -0700

    Add 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#includes <stdio.h>
+
+/*
+Write a C program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line.
+*/
+
+int main(){
+  printf("with proper grammar, but the outcome is a piece of art,/n");
+  return(0);
+}
