8de2191af21e596183fd0d07f49b3e2280fc7e51
Obed Rayo <obedrayo2356@gmail.com>
2022-10-10 14:03:32 -0700

    Add 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,14 @@
 
 /**
  * main - print a number
- * Return: Always 0 
+ * Return: Always 0
  */
 
 int main(void)
 {
 int i;
- for(i = '0'; i <= '9' ; i++){
+for (i = '0'; i <= '9' ; i++)
+{
 putchar(i);
 }
 putchar('\n');
