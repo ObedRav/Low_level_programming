ce49c8a06aa2ca94444a7a16f548ab8c9e8cd932
Obed Rayo <obedrayo2356@gmail.com>
2022-10-10 14:38:14 -0700

    Add 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints numbers base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+char number;
+char letter;
+    
+for (number = '0'; number <= '9'; number++){
+putchar(number);
+}
+    
+for (letter = 'a'; letter <= 'f'; letter++){
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
