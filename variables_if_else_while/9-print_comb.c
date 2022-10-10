33d0df7928e1e502f164e7f27c524f8ef1c61858
Obed Rayo <obedrayo2356@gmail.com>
2022-10-10 14:47:14 -0700

    Add 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints number with ,
+ * Return: Always 0
+ */
+int main(void)
+{
+int number;    
+for (number = '0'; number <= '9'; number ++)
+{
+putchar(number);
+if (number != '9'){
+putchar(',');
+}
+}
+return (0);
+}
