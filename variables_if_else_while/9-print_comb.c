61cca3a63799253bac827b6dd694f65cd8c577a1
Obed Rayo <obedrayo2356@gmail.com>
2022-10-10 15:09:49 -0700

    Add 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,9 +9,11 @@ int number;
 for (number = '0'; number <= '9'; number++)
 {
 putchar(number);
-if (number != '9')
+if (number != '9'){
 putchar(',');
 putchar(' ');
+}
+putchar('\n')
 }
 return (0);
 }
