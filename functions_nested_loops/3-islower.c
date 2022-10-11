d8d5a682688f8bdcd33e3b486b697608b622fae6
Obed Rayo <obedrayo2356@gmail.com>
2022-10-11 14:44:26 -0700

    Add 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - check if is lower or not
+ * Return: If is lower pront 1.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}else{
+return (0);
+}
+}
