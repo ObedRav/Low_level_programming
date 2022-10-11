6396ba18e80551908a54ea9771db850218593f7e
Obed Rayo <obedrayo2356@gmail.com>
2022-10-11 13:59:20 -0700

    Add 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - make 10 times the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int i, j;
+for (j = 0; j <= 9; j++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
+}
