a602cddecb387d7fbf36e0c3f740eaef7c15d561
Obed Rayo <obedrayo2356@gmail.com>
2022-10-11 08:40:55 -0700

    Add 100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
- * main - prints all posible combinations of 2 digits 
+ * main - prints all posible combinations of 2 digits
  * Return: Always 0
- */ 
-int main()
+ */
+int main(void)
 {
 int i;
 int j;
-for(i = 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
-for(j = i + 1; j <= 9; j++)
+for (j = i + 1; j <= 9; j++)
 {
-if(i == j)
+if (i == j)
 {
 continue;
 }
-putchar((i%10)+ '0');
-putchar((j%10)+ '0');
-if(i==8 && j==9)
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+if (i == 8 && j == 9)
 {
 putchar('\n');
 continue;
