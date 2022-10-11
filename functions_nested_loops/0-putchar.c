f39e97e5bb72a93823156ff8e87aff6f1af7ca15
Obed Rayo <obedrayo2356@gmail.com>
2022-10-11 10:34:18 -0700

    Add 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar 
+ * Return: always 0
+ */ 
+int main(void) 
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+return (0);
+}
