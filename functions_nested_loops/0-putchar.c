7771e1134fd6e33118ff7c7412b6d6e2b092a44c
Obed Rayo <obedrayo2356@gmail.com>
2022-10-11 10:42:35 -0700

    Add 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,16 +3,11 @@
 /**
  * main - prints _putchar 
  * Return: always 0
- */ 
+ */
+int _putchar();
+
 int main(void) 
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
+_putchar();
 return (0);
 }
