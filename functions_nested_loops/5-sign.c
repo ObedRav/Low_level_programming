f244e637d716829d26be30cf8b69f049d526dfa6
Obed Rayo <obedrayo2356@gmail.com>
2022-10-11 15:06:50 -0700

    Add 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - check the data.
+ * @n: data from user
+ * Return: tha type of data.
+ */
+
+int print_sign(int n)
+{
+if (n < 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n < 0)
+{
+_putchat('-');
+return (-1);
+}
+}
+	       
