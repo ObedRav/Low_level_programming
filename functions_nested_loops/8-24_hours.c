10b2ee5039eb626994b172cfe9302814844c89e5
Obed Rayo <obedrayo2356@gmail.com>
2022-10-12 08:54:24 -0700

    Add 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,7 +2,6 @@
 
 /**
  * jack_bauer - prints all minutes of a day
- * 
  * Return: Anything
  */
 
@@ -13,18 +12,17 @@ for (j = 0; j < 3; j++)
 {
 for (i = 0; i <= 9; i++)
 {
-if (j == 2 && i >= 4){
+if (j == 2 && i >= 4)
 continue;
-}
 for (k = 0; k < 6; k++)
 {
 for (l = 0; l <= 9; l++)
 {
-_putchar((j%10) + '0');
-_putchar((i%10) + '0');
+_putchar((j % 10) + '0');
+_putchar((i % 10) + '0');
 _putchar(':');
-_putchar((k%10) + '0');
-_putchar((l%10) + '0');
+_putchar((k % 10) + '0');
+_putchar((l % 10) + '0');
 _putchar('\n');
 }
 }
