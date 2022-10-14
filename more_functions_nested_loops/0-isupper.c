abf1307df6a4c183843dcb0f93329802cbdeb122
Obed Rayo <obedrayo2356@gmail.com>
2022-10-14 09:53:07 -0700

    Add 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,16 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - return if c is upper
+ * Return: c
+ * @c: Data from user
+ */
+
+int _isupper(int c)
+{
+	c = (c + '0')
+		if (c >= 'A' && c <= 'Z')
+		{
+			c = (c - '0')
+			return (c)
+		}
+}
