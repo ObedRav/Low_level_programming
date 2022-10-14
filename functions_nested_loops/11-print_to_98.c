0101a504a030ee0432e7abc5291eb7a0e335f3ac
Obed Rayo <obedrayo2356@gmail.com>
2022-10-13 18:11:02 -0700

    Add 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,23 +6,23 @@
  */
 
 void print_to_98(int n){
-  if(n >= 98){
-    if(n > 98){
-      for(n; n > 98; n--){
+  if (n >= 98){
+    if (n > 98){
+      for (; n > 98; n--){
 	printf("%d, ",n);
       }
     }
-    if(n == 98){
+    if (n == 98){
       printf("%d \n",n);
     }
   }
-  if(n < 97){
-    if(n < 98){
-      for(n; n < 98; n++){
+  if (n < 97){
+    if (n < 98){
+      for (; n < 98; n++){
 	printf("%d, ",n);
       }
     }
-    if(n == 98){
+    if (n == 98){
       printf("%d \n",n);
     }
   }
