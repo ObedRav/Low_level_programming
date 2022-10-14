21bcb6507a3612b50a5ebdcef3efc59d3035e120
Obed Rayo <obedrayo2356@gmail.com>
2022-10-14 14:14:36 -0700

    Add 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -5,11 +5,14 @@
  */
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
-	for (i = 1; i <= size; i++)
+	for (j = 1; j <= n; j++)
 	{
-		_putchar('#');
+		for (i = 1; i <= size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
