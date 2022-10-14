65f168697648761320e070769fe732a9b0bc27c1
Obed Rayo <obedrayo2356@gmail.com>
2022-10-13 18:12:46 -0700

    Add 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n){
       }
     }
     if (n == 98){
-      printf("%d \n",n);
+      printf("%d\n",n);
     }
   }
   if (n < 97){
@@ -23,7 +23,7 @@ void print_to_98(int n){
       }
     }
     if (n == 98){
-      printf("%d \n",n);
+      printf("%d\n",n);
     }
   }
 }
