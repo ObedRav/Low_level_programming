2d52e08b7a6ddeba21f2f04e6365c2d162faf3a2
Obed Rayo <obedrayo2356@gmail.com>
2022-10-13 17:42:24 -0700

    Add 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print numbers to 98
+ * Return: all numbers to 98
+ */
+
+void print_to_98(int n){
+  if(n >= 98){
+    if(n > 98){
+      for(n; n > 98; n--){
+	printf("%d, ",n);
+      }
+    }
+    if(n == 98){
+      printf("%d \n",n);
+    }
+  }
+  if(n < 97){
+    if(n < 98){
+      for(n; n < 98; n++){
+	printf("%d, ",n);
+      }
+    }
+    if(n == 98){
+      printf("%d \n",n);
+    }
+  }
+}
