589cd70674fa096600df4087ecd007b3d14eb992
Obed Rayo <obedrayo2356@gmail.com>
2022-10-14 12:33:27 -0700

    Add 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#incluide "main.h"
+/**
+ * print_most_numbers - print digits 0 to 9 but 2 and 4 are not presents
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+}
