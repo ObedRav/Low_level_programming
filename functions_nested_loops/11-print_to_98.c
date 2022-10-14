572f3824633806a1f0e8c4af6f297cf1fb546413
Obed Rayo <obedrayo2356@gmail.com>
2022-10-14 05:56:06 -0700

    Add 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -6,25 +6,34 @@
  * @n: number from user
  */
 
-void print_to_98(int n){
-  if (n >= 98){
-    if (n > 98){
-      for (; n > 98; n--){
-	printf("%d, ",n);
-      }
-    }
-    if (n == 98){
-      printf("%d\n",n);
-    }
-  }
-  if (n < 97){
-    if (n < 98){
-      for (; n < 98; n++){
-	printf("%d, ",n);
-      }
-    }
-    if (n == 98){
-      printf("%d\n",n);
-    }
-  }
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		if (n > 98)
+		{
+			for (; n > 98; n--)
+			{
+				printf("%d, ", n);
+			}
+		}
+		if (n == 98)
+		{
+			printf("%d\n", n);
+		}
+	}
+	if (n < 97)
+	{
+		if (n < 98)
+		{
+			for (; n < 98; n++)
+			{
+				printf("%d, ", n);
+			}
+		}
+		if (n == 98)
+		{
+			printf("%d\n", n);
+		}
+	}
 }
