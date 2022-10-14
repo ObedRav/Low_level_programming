e894f30ae6876ae01b191b311fc905d6a665ddc8
Obed Rayo <obedrayo2356@gmail.com>
2022-10-14 09:58:35 -0700

    Add 0

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,16 +3,6 @@
 
 
 int _putchar(char a);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int c);
-int print_last_digit(int c);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
+int _isupper(int c);
 
 #endif /* MAIN_H */
