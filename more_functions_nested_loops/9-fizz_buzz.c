2e5632213d26db54800e43a3b365b336adcfbf4e
Obed Rayo <obedrayo2356@gmail.com>
2022-10-14 14:40:02 -0700

    Add 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - make FizzBuzz
+ * Return: 0
  */
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0)
@@ -18,9 +19,11 @@ int main(void)
 		}
 		if ((i % 3) != 0 && (i % 5) != 0)
 		{
-			printf("%d",i);
+			printf("%d", i);
 		}
-		putchar(' ');
+		if (i <= 99)
+			putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
