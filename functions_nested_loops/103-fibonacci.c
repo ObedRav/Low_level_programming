3baa8c9925c25d69bfc5e26316e45cd6f851c0de
Obed Rayo <obedrayo2356@gmail.com>
2022-10-16 08:08:14 -0700

    Add 103

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the sum of the fibonacci < 4000000
+ * Return: Always (0)
+ */
+int main(void)
+{
+	long int i, a = 1, b = 2, suma, duplicados = 0;
+
+	for (i = 1; i <= 32; i++)
+	{
+		if (a < 4000000 && (a % 2) == 0)
+		{
+			duplicados = duplicados + a;
+		}
+		suma = a + b;
+		a = b;
+		b = suma;
+	}
+	printf("%ld \n", duplicados);
+	return (0);
+}
