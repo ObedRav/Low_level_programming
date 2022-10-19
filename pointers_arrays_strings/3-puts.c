e43180a10850a4916778f1b1f9ccc26310eaafc3
Obed Rayo <obedrayo2356@gmail.com>
2022-10-19 08:12:54 -0700

    Add 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
-
 /**
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
  */
 void _puts(char *str)
 {
-	while(*str)
+	while (*str)
 		_putchar(*str++);
+
 	_putchar('\n');
 }
