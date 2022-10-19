4296cf78bb697a0cbfcf9befa4f474f4c2aacc7c
Obed Rayo <obedrayo2356@gmail.com>
2022-10-19 07:56:02 -0700

    Add 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <string.h>
 /**
- * strlen - count chars
+ * _strlen - count chars
  * @s: data from user
+ * Return: The result
  */
 int _strlen(char *s)
 {
 	int b = strlen(s);
-	return b;
+
+	return (b);
 }
