7f119b69d97df9e383d4a2b237220510b3fd448e
Obed Rayo <obedrayo2356@gmail.com>
2022-10-28 13:12:41 -0700

    Add static-library

--- static_libraries/0-memset.c
+++ static_libraries/0-memset.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
- * _memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
- *
- * Return: A pointer to the filled memory area @s.
+ *_memset - The _memset() function fills
+ * the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ *@s:target
+ *@b: constant byte
+ *@n:number of byte
+ *Return: returns new value of target
  */
-void *_memset(void *s, int c, size_t n)
+
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-	unsigned char *memory = s, value = c;
-
-	for (i = 0; i < n; i++)
-		memory[i] = value;
-
-	return (memory);
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
---
+++ static_libraries/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - check if is lower or not
+ * @c: data from user
+ * Return: If is lower pront 1.
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+{
+return (0);
+}
+}
