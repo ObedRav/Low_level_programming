fb7ce6ad0b6c94c48a794fbe65d7b71867a18def
Obed Rayo <obedrayo2356@gmail.com>
2022-11-04 13:48:06 -0700

    Add 1

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 
 #endif
