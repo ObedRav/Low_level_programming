3798ea40d9a5376f0480734d84244865fd00fc9b
Obed Rayo <obedrayo2356@gmail.com>
2022-11-30 18:35:12 -0800

    Add 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t
+ *            list.
+ * @h: Pointer to a linked list head.
+ * Return: The number of total nodes in the linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int nodes_number = 0;
+
+	if (!h)
+	{
+		return (nodes_number);
+	}
+
+	while(h->next != NULL)
+	{
+		nodes_number += 1;
+		h = h->next;
+	}
+	nodes_number += 1;
+	return (nodes_number);
+}
