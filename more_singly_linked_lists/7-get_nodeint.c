ca017f8f60e55127f0e5b398ed4527c4ade39050
Obed Rayo <obedrayo2356@gmail.com>
2022-12-01 16:53:37 -0800

    Fix an error

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -11,11 +11,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *current;
 	unsigned int i;
 
-	if (!head)
-		return (NULL);
-
 	for (i = 0; i <= index; i++)
 	{
+		if (!head)
+			return (NULL);
+
 		current = head;
 		head = head->next;
 	}
