07e69176465d58bcafaeee70388435ead3097116
Obed Rayo <obedrayo2356@gmail.com>
2022-12-02 11:55:50 -0800

    Add 0 function

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - count chars
+ * @s: data from user
+ * Return: The result
+ */
+int _strlen(const char *s)
+{
+	int i = 0, length = 0;
+
+	while (s[i++])
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ * binary_to_uint - Converters a binary number to an unsigned integer
+ * @b: String representing a binary number
+ * Return: The converted number or 0 if b is NULL or if there are chars
+ * that are not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int dec = 0, i = 0, rem, n, j = 0, z;
+
+	if (!b)
+		return (0);
+
+	j = _strlen(b);
+
+	for (z = 0; z < j; z++)
+	{
+		if (b[z] != '0' && b[z] != '1')
+			return (0);
+	}
+
+	n = atoi(b);
+
+	while (n!=0)
+	{
+		rem = n % 10;
+		n /= 10;
+		dec += rem * pow(2, i);
+		++i;
+	}
+
+	return (dec);
+}
