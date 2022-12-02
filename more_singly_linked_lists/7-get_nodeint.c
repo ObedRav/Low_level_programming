ba62ae03e39c443609250e3e82a779593606cadf
Obed Rayo <obedrayo2356@gmail.com>
2022-12-01 16:49:13 -0800

    Add 7 function

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Returns the node at an index
+ * @head: Pointer to the first element of the list
+ * @index: Desired node at this position
+ * Return: Pointer to the desired node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+
+	for (i = 0; i <= index; i++)
+	{
+		current = head;
+		head = head->next;
+	}
+
+	return (current);
+}
