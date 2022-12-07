f3af28aac7ed5a59da45c1527e80cfce29089700
ObedRav <obedrayo2356@gmail.com>
2022-12-07 23:57:46 +0000

    Fix betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,36 +8,36 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, rd, wr;
-    char *buf;
+	int fd, rd, wr;
+	char *buf;
 
-    if (!filename)
-        return (0);
+	if (!filename)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    if (!buf)
-    {
-        close (fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
 
-    rd = read(fd, buf, letters);
-    close (fd);
-    if (rd== -1)
-    {
-        free (buf);
-        return (0);
-    }
+	rd = read(fd, buf, letters);
+	close(fd);
+	if (rd == -1)
+	{
+		free(buf);
+		return (0);
+	}
 
-    wr = write (STDOUT_FILENO,  buf, rd);
-    free (buf);
+	wr = write(STDOUT_FILENO,  buf, rd);
+	free(buf);
 
-    if (rd != wr)
-        return (0);
+	if (rd != wr)
+		return (0);
 
-    return (wr);
+	return (wr);
 }
