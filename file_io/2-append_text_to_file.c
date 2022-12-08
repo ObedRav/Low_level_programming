ad315ce14152d59fde2d8e3f4671650004a834fb
ObedRav <obedrayo2356@gmail.com>
2022-12-08 16:26:12 +0000

    Fix an error

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND);
+	fd = open(filename, O_WRONLY  | O_APPEND);
 
 	if (fd == -1)
 	{
@@ -21,10 +21,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (!text_content && (fd != -1))
+	if ((!text_content) && (fd != -1))
 	{
 		close(fd);
-		return (-1);
+		return (1);
 	}
 
 	while (text_content[len] != '\0')
