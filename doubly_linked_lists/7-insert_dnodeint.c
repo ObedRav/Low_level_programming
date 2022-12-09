57a7bb5a86f5cd7772e65ec697e6b77f8ac1cf46
ObedRav <obedrayo2356@gmail.com>
2022-12-09 15:53:46 +0000

    Add 7 function

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *prev_node = *h, *new;
 
-	if (!h || !*h)
+	if (!h)
 		return (NULL);
 
 	if (idx == 0)
