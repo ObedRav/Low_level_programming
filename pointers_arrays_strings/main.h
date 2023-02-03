59fd9e55e43ba4647611121ce6cbfdb68fd5d561
ObedRav <obedrayo2356@gmail.com>
2023-02-03 19:51:45 +0000

    Betty Style Code

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,7 +19,7 @@ char *_strcat(char *dest, char *src);
 void *_memcpy(void *dest, const void *src, size_t n);
 char *_strncat(char *dest, char *src, int n);
 void swap_int(int *a, int *b);
-char *_strchr(char *s, char c):
+char *_strchr(char *s, char c);
 int _strcmp(char *s1, char *s2);
 unsigned int _strspn(char *s, char *accept);
 void reverse_array(int *a, int n);
@@ -30,6 +30,5 @@ char *cap_string(char *str);
 char *leet(char *str);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
-
 
 #endif
