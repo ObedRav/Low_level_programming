3d264ce095f4b85fa7f48140cecaedf96a594c7f
ObedRav <obedrayo2356@gmail.com>
2023-03-27 17:02:53 +0000

    Fixing some bugs

---
+++ static_libraries/clear.sh
@@ -0,0 +1,2 @@
+rm -rf liball.a quote ./output_directory
+echo "It's clear"
--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-gcc -c ./*.c
-ar -rc liball.a ./*.o
+gcc -c ./library_functions/*.c
+mkdir -p output_directory
+mv *.o ./output_directory
+ar -rc liball.a ./output_directory/*.o
+echo "Library created successfully"
